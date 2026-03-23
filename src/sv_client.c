b1e8dd2c3c3f5612dd49b4c1c6032a2a951e7cdc
Dong <gt88129@gmail.com>
2026-03-23 22:54:56 +0700

    fix2

--- src/sv_client.c
+++ src/sv_client.c
@@ -1,31 +1,66 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <stdio.h>
 #include <winsock2.h>
+#include <string.h>
+
+#pragma comment(lib, "ws2_32.lib")
 
 typedef struct {
-    char mssv[20], hoten[50], ngaysinh[20];
+    char mssv[20];
+    char hoten[50];
+    char ngaysinh[20];
     float diem;
 } SinhVien;
 
+void clear_stdin() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
 int main(int argc, char *argv[]) {
-    if (argc != 3) return 1;
-    WSADATA wsa; WSAStartup(MAKEWORD(2, 2), &wsa);
+    if (argc != 3) {
+        printf("Huong dan: %s <IP_Server> <Port>\n", argv[0]);
+        return 1;
+    }
+
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
     SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server;
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = inet_addr(argv[1]);
     server.sin_port = htons(atoi(argv[2]));
-    connect(s, (struct sockaddr *)&server, sizeof(server));
+
+    if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        printf("Ket noi den Server that bai!\n");
+        return 1;
+    }
 
     SinhVien sv;
-    printf("MSSV: "); scanf("%s", sv.mssv); fflush(stdin);
-    printf("Ho ten: "); gets(sv.hoten);
-    printf("Ngay sinh (dd/mm/yyyy): "); scanf("%s", sv.ngaysinh);
-    printf("Diem TB: "); scanf("%f", &sv.diem);
+    memset(&sv, 0, sizeof(sv)); 
+
+    printf("--- NHAP THONG TIN SINH VIEN ---\n");
+
+    printf("MSSV: ");
+    scanf("%19s", sv.mssv);
+    clear_stdin(); 
+
+    printf("Ho ten: ");
+    fgets(sv.hoten, sizeof(sv.hoten), stdin);
+    sv.hoten[strcspn(sv.hoten, "\n")] = 0; 
+
+    printf("Ngay sinh (dd/mm/yyyy): ");
+    scanf("%19s", sv.ngaysinh);
+    clear_stdin(); 
+
+    printf("Diem trung binh: ");
+    scanf("%f", &sv.diem);
 
     send(s, (char*)&sv, sizeof(sv), 0);
-    printf("Da gui du lieu!\n");
+    printf("\n[Thanh cong] Da gui du lieu sinh vien len Server!\n");
 
-    closesocket(s); WSACleanup();
+    closesocket(s);
+    WSACleanup();
     return 0;
 }
